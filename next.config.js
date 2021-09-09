de620cbfaedbb8a6c60fda76d82b868b346c1624
DendyAR <anugerah700@gmail.com>
2021-09-09 20:26:11 +0700

    Change env

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-    env: {
-        API_URL: 'http://localhost:5000/news/api',
-        API_URL_IMG: 'http://localhost:5024',
-        ORIGIN_URL:'http://localhost:4000',
-        SECRET_COOKIE_PASSWORD: '140455yuhu'
-    },
-}
+  env: {
+    API_URL:process.env.NEXT_API_URL,
+    API_URL_IMG:process.env.NEXT_API_URL_IMG,
+    ORIGIN_URL:process.env.NEXT_ORIGIN_URL,
+    SECRET_COOKIE_PASSWORD:process.env.NEXT_SECRET_COOKIE_PASSWORD,
+  },
+};
